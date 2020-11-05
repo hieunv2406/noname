71165adbd44b1f0b55759e61d58ced3598d4a488
hieunv <hieunv2406@gmail.com>
2020-11-05 16:03:07 +0700

    hieudz

---
+++ src/main/java/com/example/noname/NonameApplication.java
@@ -0,0 +1,14 @@
+package com.example.noname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class NonameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonameApplication.class, args);
+    }
+
+}
