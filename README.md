58b48f0ce4be04790b801113862a6d2a0fa3d0c5
owl <hieunv2406@gmail.com>
2021-01-24 01:10:16 +0700

    abc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # noname
+api test: http://localhost:8080/swagger-ui.html <br>
+import sql: resources/sql/dumpData/createDatabase.sql
--- src/main/java/com/example/common/config/SpringFoxConfig.java
+++ src/main/java/com/example/common/config/SpringFoxConfig.java
@@ -2,9 +2,6 @@ package com.example.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig extends WebMvcConfigurationSupport {
+public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -35,17 +32,5 @@ public class SpringFoxConfig extends WebMvcConfigurationSupport {
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .version("1.0.0")
                 .build();
-    }
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/documentation/v2/api-docs", "/v2/api-docs").setKeepQueryParams(true);
-        registry.addRedirectViewController("/documentation/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/documentation/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/documentation/swagger-resources", "/swagger-resources");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/documentation/**").addResourceLocations("classpath:/META-INF/resources/");
     }
 }
