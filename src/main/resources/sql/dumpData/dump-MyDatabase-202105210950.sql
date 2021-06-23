d431d0b78afc61a1a406ef6142687eb5cd9deaea
hieunv <hieunv2406@gmail.com>
2021-06-23 11:18:16 +0700

    cập nhật dump DB

---
+++ src/main/resources/sql/dumpData/dump-MyDatabase-202105210950.sql
@@ -0,0 +1,160 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: MyDatabase
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `employee_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id nhân viên',
+  `username` varchar(50) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL COMMENT 'username',
+  `code` varchar(50) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL COMMENT 'mã nhân viên',
+  `full_name` varchar(255) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL COMMENT 'tên nhân viên',
+  `email` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL COMMENT 'email',
+  `gender` int DEFAULT NULL COMMENT 'giới tính',
+  `birthday` date DEFAULT NULL COMMENT 'ngành sinh',
+  `address` varchar(255) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL COMMENT 'địa chỉ',
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci COMMENT='bảng thông tin nhân viên';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'hieunv','NV2','Nguyễn Văn Nguyễn','nguyen@itsol.vn',1,'2020-05-06','Cầu Giấy - Hà Nội'),(2,'hieunv','NV2','Nguyễn Văn Nguyễn','nguyen@itsol.vn',1,'2020-05-06','Cầu Giấy - Hà Nội'),(3,'hieunv','NV2','Nguyễn Văn Nguyễn','nguyen@itsol.vn',1,'2020-05-06','Cầu Giấy - Hà Nội'),(4,'hieunv','','Nguyễn Văn Nguyễn','nguyen@itsol.vn',1,'2020-05-06','Cầu Giấy - Hà Nội');
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL COMMENT 'username',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci COMMENT='bảng thông tin phân quyền';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_MODERATOR'),(3,'ROLE_USER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table_test`
+--
+
+DROP TABLE IF EXISTS `table_test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table_test` (
+  `cot1` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `cot2` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `cot3` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table_test`
+--
+
+LOCK TABLES `table_test` WRITE;
+/*!40000 ALTER TABLE `table_test` DISABLE KEYS */;
+INSERT INTO `table_test` VALUES ('1','2','3'),('11','22','33');
+/*!40000 ALTER TABLE `table_test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` bigint NOT NULL COMMENT 'id người dùng',
+  `roles_id` bigint NOT NULL COMMENT 'id phân quyền',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci COMMENT='bảng thông tin map phân quyền';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,1,2),(2,1,1);
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id người dùng',
+  `username` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL COMMENT 'username',
+  `password` varchar(200) COLLATE utf8mb4_vietnamese_ci NOT NULL COMMENT 'mật khẩu',
+  `email` varchar(50) COLLATE utf8mb4_vietnamese_ci NOT NULL COMMENT 'email',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci COMMENT='bảng thông tin người dùng';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','$2a$10$ONc987Uf1VqQmqVBmxvyzeBD4WuCUmWvmb2VS3JFY.iOjNL20YH5u','hieunv2496@gmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'MyDatabase'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-21  9:50:12
--- src/main/resources/sql/dumpData/roles.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mydatabase.roles (
-                                     id BIGINT(20) auto_increment NOT NULL COMMENT 'id',
-                                     name varchar(50) NOT NULL COMMENT 'username',
-                                     CONSTRAINT roles_pk PRIMARY KEY (id)
-)
-    ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_vietnamese_ci
-COMMENT='bảng thông tin phân quyền';
